d18353bf6a8419d2792dd3aeb2a9d658b55a1248
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-06-23 18:42:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
     //Write your code here
-
-
+	let s = document.querySelectorAll('p>strong');
+	s.forEach((ele)=>{
+		ele.className = "high";
+	})
+	// console.log(s);
+	// return false;
 }
 
 
 function return_normal() {
     //Write your code here
-
+	let s = document.querySelectorAll('p>strong');
+		s.forEach((ele)=>{
+			ele.className = "def";
+		})
     
 }
